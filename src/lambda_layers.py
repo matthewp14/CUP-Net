9b12c81e4ef9e59142586cd1469897814c832ade
matthewp14 <31370946+matthewp14@users.noreply.github.com>
2020-02-11 11:53:33 -0500

    clean up lambda file. got rid of testing functions

--- src/lambda_layers.py
+++ src/lambda_layers.py
@@ -3,16 +3,7 @@
 # Code to test the implementation of
 # Lambda layers for CNN
 
-#from __future__ import print_function
-import keras
-from keras import layers
-from keras.layers import Lambda
-from keras import backend as K
 import numpy as np
-from keras.datasets import mnist
-
-import cv2
-from matplotlib import pyplot as plt
 
 """
 Streak: Simulation of streak camera. 
@@ -57,31 +48,3 @@ def integrate_ims_output_shape(input):
     dims = np.shape(input)
     return (1, dims[1],dims[2],dims[3])
     
-    
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-X_train = X_train.reshape(60000, 28, 28, 1)
-X_test = X_test.reshape(10000, 28, 28, 1)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-#print(X_train.shape[0], 'train samples')
-#print(X_test.shape[0], 'test samples')
-
-X_train = X_train[0:30]
-
-s_t = streak(X_train)
-
-#print(np.shape(s_t))
-plt.figure(0)
-plt.imshow(np.reshape(s_t[5],(-1,28)), interpolation='nearest')
-plt.figure(1)
-plt.imshow(np.reshape(s_t[0],(-1,28)), interpolation='nearest')
-
-
-
-s_t_final=integrate_ims(s_t)
-
-plt.figure(3)
-plt.imshow(np.reshape(s_t_final,(-1,28)),interpolation='nearest')
