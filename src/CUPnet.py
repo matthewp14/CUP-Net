5acf598bbba3e8a9389be85811c77b8d5fd5859d
matthewp14 <matthewp14@gmail.com>
2020-02-23 20:39:57 -0500

    timeDistributed everything

--- src/CUPnet.py
+++ src/CUPnet.py
@@ -7,7 +7,7 @@ Created on Wed Feb  5 16:05:10 2020
 
 NOTE: For now we are sticking to videos with dims: 30x30x30 (x,y,t) to limit toal parameters
 """
-import tensorflow
+import tensorflow as tf
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 import tensorflow.keras.backend as K
@@ -59,6 +59,8 @@ momentum = 0.9
 p1 = 0.25
 p2 = 0.5
 
+data_shape = tf.TensorShape([30,30,30,1])
+
 # def cupnet(input_shape = (30,30,30,1)):
 #     model = Sequential()
     
@@ -81,56 +83,57 @@ p2 = 0.5
 #     model.add
 
 
-def unet(pretrained_weights = None,input_size = (30,30,30,1)):
-    inputs = Input(input_size)
+def unet(pretrained_weights = None,input_shape = data_shape,input_size = (30,30,30,1)):
+    inputs = Input(shape=data_shape,batch_size=batch_size)
     bin_conv1 = TimeDistributed(BinaryConv2D(1, kernel_size=(30,30), input_shape=input_size,
                        data_format='channels_last',
                        H=H, kernel_lr_multiplier=kernel_lr_multiplier,
                        padding='same', use_bias=use_bias, name='bin_conv_1'))(inputs)
-    s = Lambda(streak, output_shape = streak_output_shape)(bin_conv1)
+    s = Lambda(tf.py_function(streak,), output_shape = streak_output_shape)(bin_conv1)
     i = Lambda(integrate_ims, output_shape = integrate_ims_output_shape) (s)
     f = Flatten()(i)
-    dense1 = Dense(27000, activatiion = 'relu')(f)
+    dense1 = Dense(27000, activation = 'relu')(f)
     resh = Reshape((30,30,30,1))(dense1)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(resh)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+    conv1 = TimeDistributed(Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(resh)
+    conv1 = TimeDistributed(Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv1)
+    pool1 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv1)
+                            
+    conv2 = TimeDistributed(Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(pool1)
+    conv2 = TimeDistributed(Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv2)
+    pool2 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv2)
+    conv3 = TimeDistributed(Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(pool2)
+    conv3 = TimeDistributed(Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv3)
+    pool3 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv3)
+    conv4 = TimeDistributed(Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(pool3)
+    conv4 = TimeDistributed(Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv4)
+    drop4 = TimeDistributed(Dropout(0.5))(conv4)
+    pool4 = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(drop4)
 
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
+    conv5 = TimeDistributed(Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(pool4)
+    conv5 = TimeDistributed(Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv5)
+    drop5 = TimeDistributed(Dropout(0.5))(conv5)
 
-    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-    merge6 = concatenate([drop4,up6], axis = 3)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+    up6 = TimeDistributed(Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(drop5))
+    merge6 = TimeDistributed(concatenate([drop4,up6], axis = 3))
+    conv6 = TimeDistributed(Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(merge6)
+    conv6 = TimeDistributed(Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv6)
 
-    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-    merge7 = concatenate([conv3,up7], axis = 3)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+    up7 = TimeDistributed(Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(conv6))
+    merge7 = TimeDistributed(concatenate([conv3,up7], axis = 3))
+    conv7 = TimeDistributed(Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(merge7)
+    conv7 = TimeDistributed(Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv7)
 
-    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-    merge8 = concatenate([conv2,up8], axis = 3)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    up8 = TimeDistributed(Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(conv7))
+    merge8 = TimeDistributed(concatenate([conv2,up8], axis = 3))
+    conv8 = TimeDistributed(Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(merge8)
+    conv8 = TimeDistributed(Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv8)
 
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    merge9 = concatenate([conv1,up9], axis = 3)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+    up9 = TimeDistributed(Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2)))(conv8))
+    merge9 = TimeDistributed(concatenate([conv1,up9], axis = 3))
+    conv9 = TimeDistributed(Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(merge9)
+    conv9 = TimeDistributed(Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv9)
+    conv9 = TimeDistributed(Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal'))(conv9)
+    conv10 = TimeDistributed(Conv2D(1, 1, activation = 'sigmoid'))(conv9)
 
     model = Model(input = inputs, output = conv10)
     
