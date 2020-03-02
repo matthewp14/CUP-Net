b3628770a5f437807f5a7d3a3f3924b8c6c42b35
matthewp14 <matthewp14@gmail.com>
2020-03-02 10:44:18 -0500

    gradient tracking working

--- src/lambda_layers.py
+++ src/lambda_layers.py
@@ -17,16 +17,34 @@ Output:
     streaked np.array. new y dimension = original_y + total_images
 """
 
+# @tf.function
+# def streak(x):
+#     output_shape = streak_output_shape(np.shape(x))
+#     streak_tensor = np.zeros(output_shape)
+#     # streak_tensor = tf.Variable(streak_tensor,trainable=False,dtype ="float32")
+#     streak_tensor = K.variable(streak_tensor,dtype="float32")
+#     for i in range(output_shape[0]):
+#         for j in range(output_shape[1]):
+#             im = x[i,j,:,:,:]
+#             streak_tensor[i,j,j:(output_shape[3]+j),:,:].assign(im)
+#     return streak_tensor
+
+
 @tf.function
 def streak(x):
     output_shape = streak_output_shape(np.shape(x))
-    streak_tensor = np.zeros(output_shape)
-    streak_tensor = tf.Variable(streak_tensor,trainable=False,dtype ="float32")
-    for i in range(output_shape[0]):
-        for j in range(output_shape[1]):
-            im = x[i,j,:,:,:]
-            streak_tensor[i,j,j:(output_shape[3]+j),:,:].assign(im)
-    return streak_tensor
+    paddings = tf.constant([[0,0],[0,0],[0,output_shape[1]],[0,0],[0,0]])
+    x = tf.pad(x,paddings) # pad each frame with zeros below the data 
+    streak_starter = np.zeros((output_shape[1],output_shape[2],output_shape[2])) # cant have color channel for np.fill_diagonal
+    
+    for i in range(output_shape[1]):
+        np.fill_diagonal(streak_starter[i,i+1:,:],1)
+    # streak_starter = np.reshape(streak_starter, (output_shape[1],output_shape[2],output_shape[2],1))
+    # streak = streak_starter
+    streak_starter = tf.convert_to_tensor(streak_starter,dtype="float32")
+    x = tf.reshape(x,(np.shape(x)[:-1]))
+    outputs = tf.linalg.matmul(streak_starter,x)
+    return tf.reshape(outputs, output_shape)
     
 def streak_output_shape(input_shape):
     shape = list(input_shape)
